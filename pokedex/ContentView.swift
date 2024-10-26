f4d6d4850710d1abffbeafa53673dea8f0e7f3a8
Juan Kuga <juankp3@gmail.com>
2024-10-25 22:55:32 -0500

    base

--- pokedex/ContentView.swift
+++ pokedex/ContentView.swift
@@ -7,14 +7,41 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @Binding var document: pokedexDocument
-
+struct PokedexView: View {
+    @State private var pokemonName: String = "" // Variable de estado para el nombre del Pokémon ingresado
+    
     var body: some View {
-        TextEditor(text: $document.text)
+        VStack {
+            Text("Pokedex")
+                .font(.largeTitle)
+                .padding()
+            
+            TextField("Ingresa el nombre del Pokémon", text: $pokemonName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Button(action: {
+                // Acción del botón para leer el texto ingresado
+                print("Nombre del Pokémon ingresado: \(pokemonName)")
+            }) {
+                Text("Buscar")
+                    .font(.headline)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .padding()
     }
 }
 
-#Preview {
-    ContentView(document: .constant(pokedexDocument()))
+struct PokedexView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokedexView()
+    }
 }
--- pokedex/pokedexApp.swift
+++ pokedex/pokedexApp.swift
@@ -10,8 +10,14 @@ import SwiftUI
 @main
 struct pokedexApp: App {
     var body: some Scene {
+        WindowGroup{
+            PokedexView()
+        }
+        /**
         DocumentGroup(newDocument: pokedexDocument()) { file in
             ContentView(document: file.$document)
         }
+         */
+        
     }
 }
