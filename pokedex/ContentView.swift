ed5d233bbb7ca738050a5601f036fc17f6118e90
Juan Kuga <juankp3@gmail.com>
2024-10-26 08:30:01 -0500

    personalizando audio

--- pokedex/ContentView.swift
+++ pokedex/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import AVFoundation
 
 struct PokedexView: View {
-    @State private var pokemonName: String = "" // Variable de estado para el nombre del Pokémon ingresado
+    @State private var pokemonName: String = "Juan Kuga. Es un genio y le gusta programar" // Variable de estado para el nombre del Pokémon ingresado
     private let speechSynthesizer = AVSpeechSynthesizer() // Sintetizador de voz
     
     var body: some View {
@@ -41,11 +41,13 @@ struct PokedexView: View {
         .padding()
     }
     
-    // Función para leer el texto en voz alta
+    // Función para leer el texto en voz alta con una voz robótica
     func speakText(_ text: String) {
         let utterance = AVSpeechUtterance(string: text)
-        utterance.voice = AVSpeechSynthesisVoice(language: "es-ES") // Configura la voz en español (de España)
-        utterance.rate = 0.5 // Velocidad de la voz
+        utterance.voice = AVSpeechSynthesisVoice(language: "es-PE") // Puedes probar también "en-GB" o "en-AU" para variar
+        utterance.rate = 0.5 // Velocidad más lenta para una entonación robótica
+        utterance.pitchMultiplier = 1.0 // Reduce el tono para una voz más profunda y mecánica
+        utterance.volume = 1.0 // Ajusta el volumen a un nivel más bajo, si es necesario
         
         speechSynthesizer.speak(utterance)
     }
@@ -56,4 +58,3 @@ struct PokedexView_Previews: PreviewProvider {
         PokedexView()
     }
 }
-
